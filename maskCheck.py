0fbe1dcec8187eae9645a8cfb7cffb3c202154f6
bighead <zcgtaipei@gmail.com>
2020-03-09 10:55:40 +0800

    Add files via upload

--- maskCheck.py
+++ maskCheck.py
@@ -1,7 +1,16 @@
 import csv
-import requests
+import requests as s
 url = "https://data.nhi.gov.tw/resource/mask/maskdata.csv"
-with requests.Session() as s:
-    cr = csv.reader(s.get(url).content.decode('utf-8').splitlines(), delimiter=',')
-    [print(row) for row in list(cr)]
 
+cr = csv.reader(s.Session().get(url).content.decode('utf-8').splitlines(), delimiter=',')
+[print(row) for row in list(cr)]
+
+
+
+'''
+import csv
+import requests
+with requests.Session() as s:
+	cr = csv.reader(s.get(url).content.decode('utf-8').splitlines(), delimiter=',')
+	[print(row) for row in list(cr)]
+'''
